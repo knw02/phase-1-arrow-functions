451e0404e3e6a34634e386917399a783a74bef4d
Kalie <kwatsonnov2002@gmail.com>
2023-11-02 16:20:52 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,7 @@
+function divide () {
+    const result = 2000 / 100;
+    return result;
+}
+const square = (parameter) => parameter * parameter;
+const add = (paremeter1, paremeter2) => paremeter1 + paremeter2;
+add(2,3)
